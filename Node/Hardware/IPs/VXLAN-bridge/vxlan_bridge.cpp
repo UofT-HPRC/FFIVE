c0f36b26a4e62d7b9843a8562e9d6c6712479bbd
juancamilovega <camilo55@rogers.com>
2021-07-09 12:11:48 -0400

    UNTESTED changing the code to have a much lower II
    Change to resource pragma and restructuring code order

--- Node/Hardware/IPs/VXLAN-bridge/vxlan_bridge.cpp
+++ Node/Hardware/IPs/VXLAN-bridge/vxlan_bridge.cpp
@@ -10,12 +10,28 @@
 #define PACKET_END 2
 #define DROP_PACKET 3
 
+struct net_data
+{
+	ap_uint<512> data;
+	ap_uint<64> keep;
+	ap_uint<1> last;
+};
+struct net_data_out
+{
+	ap_uint<512> data;
+	ap_uint<64> keep;
+	ap_uint<1> last;
+	ap_uint<16> id;
+	ap_uint<16> dest;
+	ap_uint<32> user;
+};
+
 void vxlan_bridge
 (
-    hls::stream<hls::axis<ap_uint<512>, 0, 0, 0>> &network_in,
-	hls::stream<hls::axis<ap_uint<512>, 32, 16, 16>> &network_out,
-	hls::stream<hls::axis<ap_uint<512>, 0, 0, 0>> &user_in,
-	hls::stream<hls::axis<ap_uint<512>, 0, 0, 0>> &user_out,
+    hls::stream<net_data> &network_in,
+	hls::stream<net_data_out> &network_out,
+	hls::stream<net_data> &user_in,
+	hls::stream<net_data> &user_out,
 	ap_uint<24> vni,
 	ap_uint<32> ip_addr,
 	ap_uint<16> local_port,
@@ -24,21 +40,24 @@ void vxlan_bridge
 {
 
 #pragma HLS INTERFACE ap_ctrl_none port=return
-#pragma HLS DATAFLOW
 
-#pragma HLS INTERFACE axis port=network_in
-#pragma HLS INTERFACE axis port=network_out
-#pragma HLS INTERFACE axis port=user_in
-#pragma HLS INTERFACE axis port=user_out
+#pragma HLS resource core=AXI4Stream variable=network_in
+#pragma HLS DATA_PACK variable=network_in
+#pragma HLS resource core=AXI4Stream variable=network_out
+#pragma HLS DATA_PACK variable=network_out
+#pragma HLS resource core=AXI4Stream variable=user_in
+#pragma HLS DATA_PACK variable=user_in
+#pragma HLS resource core=AXI4Stream variable=user_out
+#pragma HLS DATA_PACK variable=user_out
 
 	static ap_uint<4> net_input_stage = PACKET_START;
 	static ap_uint<4> usr_input_stage = PACKET_START;
-	static hls::axis<ap_uint<512>, 0, 0, 0> net_input_overflow;
-	static hls::axis<ap_uint<512>, 0, 0, 0> usr_input_overflow;
-	hls::axis<ap_uint<512>, 0, 0, 0> net_input_buf;
-	hls::axis<ap_uint<512>, 0, 0, 0> usr_input_buf;
-	hls::axis<ap_uint<512>, 32, 16, 16> net_output_buf;
-	hls::axis<ap_uint<512>, 0, 0, 0> usr_output_buf;
+	static net_data net_input_overflow;
+	static net_data usr_input_overflow;
+	net_data net_input_buf;
+	net_data usr_input_buf;
+	net_data_out net_output_buf;
+	net_data usr_output_buf;
 	net_output_buf.id = local_port;
 	net_output_buf.dest = remote_port;
 	net_output_buf.user = ip_addr;
@@ -48,32 +67,28 @@ void vxlan_bridge
 		if (!network_in.empty() && !user_out.full())
 		{
 			net_input_buf = network_in.read();
+			usr_output_buf.data.range(511,64)=net_input_buf.data.range(447,0);
+			usr_output_buf.data.range(63,0)=0;
+			usr_output_buf.keep.range(63,8)=net_input_buf.keep.range(55,0);
+			usr_output_buf.keep.range(7,0)=0;
+			usr_output_buf.last = 1;
 			if (net_input_buf.data.range(479,456)==vni)
 			{
 				if (net_input_buf.last == 1)
 				{
-					usr_output_buf.data.range(511,64)=net_input_buf.data.range(447,0);
-					usr_output_buf.data.range(63,0)=0;
-					usr_output_buf.keep.range(63,8)=net_input_buf.keep.range(55,0);
-					usr_output_buf.keep.range(7,0)=0;
-					usr_output_buf.last = 1;
-					user_out.write(usr_output_buf);
 					net_input_stage = PACKET_START;
+					user_out.write(usr_output_buf);
 				}
 				else
 				{
-					net_input_overflow = net_input_buf;
 					net_input_stage = PACKET_MIDDLE;
 				}
 			}
-			else if (net_input_buf.last != 1)
-			{
-				net_input_stage = DROP_PACKET;
-			}
 			else
 			{
-				net_input_stage = PACKET_START;
+				net_input_stage = (net_input_buf.last == 1) ? PACKET_START : DROP_PACKET;
 			}
+			net_input_overflow = net_input_buf;
 		}
 		break;
 	case DROP_PACKET:
@@ -91,6 +106,7 @@ void vxlan_bridge
 			usr_output_buf.data.range(63,0)=net_input_buf.data.range(511,448);
 			usr_output_buf.keep.range(63,8)=net_input_overflow.keep.range(55,0);
 			usr_output_buf.keep.range(7,0)=net_input_buf.keep.range(63,56);
+			user_out.write(usr_output_buf);
 			if (net_input_buf.last == 0)
 			{
 				net_input_stage = PACKET_MIDDLE;
@@ -108,7 +124,7 @@ void vxlan_bridge
 				usr_output_buf.last = 0;
 				net_input_overflow = net_input_buf;
 			}
-			user_out.write(usr_output_buf);
+
 		}
 		break;
 	case PACKET_END:
@@ -123,26 +139,15 @@ void vxlan_bridge
 			if (!network_in.empty())
 			{
 				net_input_buf = network_in.read();
-				net_input_overflow = net_input_buf;
 				if (net_input_buf.data.range(479,456)==vni)
 				{
-					if (net_input_buf.last == 1)
-					{
-						net_input_stage = PACKET_END;
-					}
-					else
-					{
-						net_input_stage = PACKET_MIDDLE;
-					}
-				}
-				else if (net_input_buf.last == 1)
-				{
-					net_input_stage = PACKET_START;
+					net_input_stage = (net_input_buf.last == 1) ? PACKET_END : PACKET_MIDDLE;
 				}
 				else
 				{
-					net_input_stage = DROP_PACKET;
+					net_input_stage = (net_input_buf.last == 1) ? PACKET_START : DROP_PACKET;
 				}
+				net_input_overflow = net_input_buf;
 			}
 			else
 			{
@@ -190,7 +195,6 @@ void vxlan_bridge
 			net_output_buf.data.range(447,0) = usr_input_buf.data.range(511,64);
 			net_output_buf.keep.range(63,56)=usr_input_overflow.keep.range(7,0);
 			net_output_buf.keep.range(55,0)=usr_input_buf.keep.range(63,8);
-			usr_input_overflow = usr_input_buf;
 			if (usr_input_buf.last == 0)
 			{
 				net_output_buf.last = 0;
@@ -207,6 +211,7 @@ void vxlan_bridge
 				usr_input_stage = PACKET_END;
 			}
 			network_out.write(net_output_buf);
+			usr_input_overflow = usr_input_buf;
 		}
 		break;
 	case PACKET_END:
